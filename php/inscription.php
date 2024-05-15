4664f4ec0619623225704b52cf4298e727287911
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-05-15 16:12:27 +0200

    commit6

---
+++ php/inscription.php
@@ -0,0 +1,17 @@
+<?php include_once 'cnx.php'; $_GET['current_page'] = "inscription";?>
+<link rel="stylesheet" href="../css/common.css">
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie Shop - Inscription</title>
+    <link rel="stylesheet" href="../css/inscription.css">
+</head>
+<?php include 'header.php'; ?>
+<body>
+    
+</body>
+<?php include 'footer.php'; ?>
+</html>

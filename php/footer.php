bef1a1db97ef94236d34c6b60b7746f1fbadd803
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-05-15 16:08:54 +0200

    commit5

--- php/footer.php
+++ php/footer.php
@@ -1,1 +1,16 @@
 <link rel="stylesheet" href="../css/footer.css">
+<footer>
+    <div class="links">
+        <a href="#">A propos</a>
+        <a href="#">Mentions légales</a>
+        <a href="#">Politique de cookies</a>
+    </div>
+    <div class="countries">
+        <p>France</p>
+        <p>United States</p>
+        <p>Spain</p>
+        <p>Germany</p>
+        <p>United Kingdom</p>
+        <p>Italy</p>
+    </div>
+</footer>

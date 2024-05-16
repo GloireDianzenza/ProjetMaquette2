3332a385820a12ba12dc2dd3da9bd3b60b6d074a
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-05-16 10:45:56 +0200

    accueil 1

--- php/accueil.php
+++ php/accueil.php
@@ -1,11 +1,28 @@
+<?php include_once 'cnx.php'; $_GET['current_page'] = "accueil";?>
+<link rel="stylesheet" href="../css/common.css">
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie Shop - Accueil</title>
+    <link rel="stylesheet" href="../css/accueil.css">
 </head>
+<?php include 'header.php' ?>
 <body>
-    
+    <section class="s1">
+        <div class="left">
+            <img src="../img/sun.png" alt="">
+        </div>
+        <div class="right">
+            <h1>The Matinee</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nostrum deserunt ullam minus id inventore velit quas, qui sapiente. Earum sit hic ut tempore suscipit ad impedit aperiam, accusantium praesentium.</p>
+        </div>
+    </section>
+    <section class="s2">
+        <h2>Liste de films aléatoires</h2>
+    </section>
 </body>
+<?php include 'footer.php' ?>
 </html>
--- php/connexion.php
+++ php/connexion.php
@@ -1,11 +1,52 @@
+<?php include_once 'cnx.php'; $_GET['current_page'] = "connexion";?>
+<link rel="stylesheet" href="../css/common.css">
+
+<?php
+    if(isset($_GET['email']) && isset($_GET['password']))
+    {
+        $mail = $_GET['email'];
+        $pwd = $_GET['password'];
+        $account = $cnx->query("SELECT * FROM account where email = '".$mail."' AND password = '".$pwd."'");
+        $account = $account->fetch(PDO::FETCH_ASSOC);
+        if($account)
+        {
+            $id = $account['id'];
+            header("Location:accueil.php?id=".$id);
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie Shop - Connexion</title>
+    <link rel="stylesheet" href="../css/connexion.css">
 </head>
+<?php include 'header.php'; ?>
 <body>
-    
+    <h1 class="title">Se connecter</h1>
+    <form action="connexion.php">
+        <div class="input-field">
+            <label for="">Adresse email</label>
+            <input type="email" name="email" placeholder="Email" required>
+        </div>
+        <div class="input-field">
+            <label for="">Mot de passe</label>
+            <input type="password" name="password" placeholder="Mot de passe" required>
+        </div>
+        <button type="submit">Se connecter</button>
+    </form>
+    <?php
+        if(isset($_GET['email']) && isset($_GET['password']))
+        {
+            if(!isset($account) || !$account)
+            {
+                echo "<script>alert('Compte inexistant ou mdp erroné');</script>";
+            }
+        }
+    ?>
 </body>
+<?php include 'footer.php'; ?>
 </html>
